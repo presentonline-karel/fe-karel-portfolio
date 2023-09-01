c1e172e283611b7b02c95214585991c29be15f60
kareldecoene <info.presentonline@gmail.com>
2023-09-01 12:03:06 +0200

    Started on fe basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default async function Home() {
 
   const api = "http://be-karel-portfolio.int/api/query";
@@ -15,7 +13,12 @@ export default async function Home() {
   const response = await fetch(api, {
     method: "post",
     body: JSON.stringify({
-      query: "site.children",
+      query: "page('projects').children",
+      select: {
+        "url": true,
+        "longTitle": true,
+        "description": true
+      }
     }),
     headers,
   });
@@ -24,8 +27,17 @@ export default async function Home() {
   console.log(json);
 
   return (
-    <div className="w-full bg-slate-500">
-      <h1>Test</h1>
+    <div className="w-full">
+      <h1 className="h h1">H1 test title</h1>
+      <h2 className="h h2">H2 test title</h2>
+      <h3 className="h h3">H3 test title</h3>
+      <h4 className="h h4">H4 test title</h4>
+      <h5 className="h h5">H5 test title</h5>
+      <h6 className="h h6">H6 test title</h6>
+
+      <button className="btn btn-primary">
+        Home
+      </button>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,101 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				"prim-100": "#FFF7EB",
+				"prim-200": "#FFE7C2",
+				"prim-300": "#FFD699",
+				"prim-400": "#FFC670",
+				"prim-500": "#FFB647",
+				"prim-600": "#FCAC22",
+				"prim-700": "#FBA40E",
+				"prim-800": "#F19A04",
+				"prim-900": "#DD8D03",
+				"prim-1000": "#C98003",
+				"neutrals-100": "#FFFFFF",
+				"neutrals-200": "#F8FBFF",
+				"neutrals-300": "#F4F5F6",
+				"neutrals-400": "#E9EBED",
+				"neutrals-500": "#DDE0E3",
+				"neutrals-600": "#C7CCD1",
+				"neutrals-700": "#B0B8BF",
+				"neutrals-800": "#9AA4AC",
+				"neutrals-900": "#84909A",
+				"neutrals-1000": "#6E7C87",
+				"neutrals-1100": "#5C6770",
+				"neutrals-1200": "#49525A",
+				"neutrals-1300": "#40484F",
+				"neutrals-1400": "#2E3338",
+				"neutrals-1500": "#25292D",
+				"neutrals-1600": "#222222",
+				"error-100": "#FCE6E6",
+				"error-300": "#F08380",
+				"error-500": "#E10600",
+				"success-100": "#DBF0D9",
+				"success-300": "#A5DAA1",
+				"success-500": "#4BB543",
+				"warning-100": "#FFF1E7",
+				"warning-300": "#FDBA87",
+				"warning-500": "#FB750E",
+				"info-100": "#E7F1FB",
+				"info-300": "#88BBED",
+				"info-500": "#1077DA",
+			},
+      fontFamily: {
+        kanit: ["var(--font-kanit)"],
       },
-    },
-  },
-  plugins: [],
-}
-export default config
+      lineHeight: {
+        "11": "2.75rem",
+        "12": "3rem",
+        "13": "3.25rem",
+        "14": "3.5rem",
+        "15": "3.75rem",
+        "16": "4rem",
+        "17": "4.25rem",
+        "18": "4.5rem",
+        "18px": "18px",
+        "26px": "26px",
+        "30px": "30px",
+        "38px": "38px",
+      }
+		},
+		fontSize: {
+      "12": "12px",
+      "14": "14px",
+      "16": "16px",
+      "18": "18px",
+      "20": "20px",
+      "24": "24px",
+      "28": "28px",
+      "32": "32px",
+      "36": "36px",
+      "40": "40px",
+      "44": "44px",
+      "48": "48px",
+      "56": "56px",
+      "64": "64px",
+      "72": "72px",
+		},
+		screens: {
+			sm: "640px",
+			md: "1024px",
+			lg: "1280px",
+			xl: "1440px",
+			hd: "1920px",
+		},
+	},
+	plugins: [],
+};
+export default config;
