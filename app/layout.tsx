55875e37e5619024724c84e507259d188c4317cc
kareldecoene <info.presentonline@gmail.com>
2023-11-18 15:53:12 +0100

    Another test for the analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,9 +65,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
+              gtag('config', 'G-CELKBJZVJM');
             `,
           }}
         />
